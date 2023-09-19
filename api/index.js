4ec35200288c8890c214ce20f66afd6dacedd8c7
Sourabh Kothari <jainjain390@gmail.com>
2023-09-20 01:56:52 +0530

    minor changes

--- api/index.js
+++ api/index.js
@@ -22,21 +22,21 @@ mongoose
 .catch(
     (err)=>{console.log(err)}
 );
-const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
- }
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+//  }
  
-// app.use(cors(
-//     {
-//         origin: ["https://slayturn-server.vercel.app/"],
-//         methods: ["POST","GET"],
-//         credentials: true
-//     }
+app.use(cors(
+    {
+        origin: ["https://slayturn-server.vercel.app/"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
 
-// ));
-app.use(cors(corsOptions))
+));
+// app.use(cors(corsOptions))
 app.use(express.json())
 app.use("/api/auth",authRoute)
 app.use("/api/users", userRoute)
