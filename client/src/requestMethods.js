5c939bddfefb14924091e7d83878ae3ad828096b
Sourabh Kothari <jainjain390@gmail.com>
2023-09-19 22:57:51 +0530

    minor fix

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "https://slayturn-server.vercel.app/";
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
 
-const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user ;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
