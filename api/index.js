e4bd93fc2cc53d1afa686e4ac8adc443a51900ae
Sourabh Kothari <jainjain390@gmail.com>
2023-09-20 10:35:58 +0530

    modified

--- api/index.js
+++ api/index.js
@@ -28,15 +28,16 @@ mongoose
 //     optionSuccessStatus: 200,
 //  }
  
-app.use(cors(
-    {
-        origin: ["https://slayturn-server.vercel.app/"],
-        methods: ["POST","GET"],
-        credentials: true
-    }
+// app.use(cors(
+//     {
+//         origin: ["https://slayturn-server.vercel.app/"],
+//         methods: ["POST","GET"],
+//         credentials: true
+//     }
 
-));
+// ));
 // app.use(cors(corsOptions))
+app.use(cors()) 
 app.use(express.json())
 app.use("/api/auth",authRoute)
 app.use("/api/users", userRoute)
