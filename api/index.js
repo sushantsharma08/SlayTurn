8e220b5af4984212254fac5204c988f456cb5c7e
Sourabh Kothari <jainjain390@gmail.com>
2023-09-23 17:29:35 +0530

    cors policy fix

--- api/index.js
+++ api/index.js
@@ -22,21 +22,21 @@ mongoose
 .catch(
     (err)=>{console.log(err)}
 );
-// const corsOptions = {
-//     origin: '*',
-//     credentials: true,            //access-control-allow-credentials:true
-//     optionSuccessStatus: 200,
-//  }
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+ }
  
-app.use(cors(
-    {
-        origin: ["https://slayturn-api.onrender.com"],
-        methods: ["GET","POST"],
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         origin: ["https://slayturn-api.onrender.com"],
+//         methods: ["GET","POST"],
+//         credentials: true
+//     }
+// ));
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 // app.use(cors()) 
 app.use(express.json())
 app.use("/api/auth",authRoute)
