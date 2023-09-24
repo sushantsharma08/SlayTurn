4e4a0c8d99279c8bef02bb0d848174fed21888d7
Sourabh Kothari <jainjain390@gmail.com>
2023-09-24 12:46:45 +0530

    update: added login functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -11,7 +11,7 @@ const Container = styled.div`
         rgba(255, 255, 255, 0.5),
         rgba(255, 255, 255, 0.5)
     ),
-    url("https://images.pexels.com/photos/845434/pexels-photo-845434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+    url("https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80")
         center;
     background-size: cover;
     display: flex;
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://slayturn-api.onrender.com/api/";
+const BASE_URL = "https://slayturn-api.onrender.com/api";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
