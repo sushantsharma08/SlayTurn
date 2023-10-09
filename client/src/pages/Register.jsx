91232557636533bfe99c569b7e627c5870f08e9f
Sourabh Kothari <jainjain390@gmail.com>
2023-10-09 18:21:27 +0530

    fix: after registration user redirect to login pg

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -89,8 +89,8 @@ export default function Register() {
     
         const res = await publicRequest.post("/auth/register", data);
         console.log(res.status);
-        // if (res.status === 201) window.location.replace("/login");
-        if(res.status === 201)  window.location = "/login"
+        if (res.status === 201) window.location.replace("https://slayturn-api.onrender.com/api/login");
+        // if(res.status === 201)  window.location = "/login"
       };
       
   return (
